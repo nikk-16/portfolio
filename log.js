4a65f68974677b9477775de6df068fae5f7cb1fc
nikk-16 <nikhilchaurasiya16401@gmail.com>
2023-06-16 06:33:12 +0530

    Updated Portfolio

---
+++ log.js
@@ -0,0 +1,34 @@
+let toggle = document.querySelector('.toggle');
+let topbar = document.querySelector('.topbar');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+toggle.onclick = function () {
+   toggle.classList.toggle('active');
+   topbar.classList.toggle('active');
+   navigation.classList.toggle('active');
+   main.classList.toggle('active');
+}
+
+// education 
+const tabsContainer = document.querySelector(".about-tabs"),
+education = document.querySelector(".education-experience");
+
+tabsContainer.addEventListener("click" , (e) =>
+{
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        //  console.log(target);
+        education.querySelector(".tab-content.active").classList.remove("active");
+        education.querySelector(target).classList.add("active");
+    }
+});
+
+// fix menu active class issue for mobile devices
+function toggleMenu(){
+   let navigation = document.querySelector('.navigation');
+   let main = document.querySelector('.main');
+   navigation.classList.remove('active');
+   main.classList.remove('active');
+}
